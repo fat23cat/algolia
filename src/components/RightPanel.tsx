c845b5a7c65b3280decb411d304744328501d1d8
Pavel Sidorenko <fat.cat.1703@gmail.com>
2025-10-06 15:39:01 +0200

    wip

--- src/components/RightPanel.tsx
+++ src/components/RightPanel.tsx
@@ -1,4 +1,4 @@
-import { Center, For, ScrollArea, VStack } from "@chakra-ui/react";
+import { Center, Flex, For, ScrollArea, Text, VStack } from "@chakra-ui/react";
 import { useEffect, useState, type FC } from "react";
 import { useConfigure, useHits, useInstantSearch } from "react-instantsearch";
 import type { SearchState } from "../types/types";
@@ -29,17 +29,37 @@ export const RightPanel: FC<RightPanelProps> = ({ searchState }) => {
   }
 
   return (
-    <ScrollArea.Root variant="always">
-      <ScrollArea.Viewport>
-        <ScrollArea.Content paddingEnd="3" textStyle="sm">
-          <VStack gap="16px" align="start" w="100%">
-            <For each={frozenItems}>
-              {(item) => <div key={item.objectID}>{item.name}</div>}
-            </For>
-          </VStack>
-        </ScrollArea.Content>
-      </ScrollArea.Viewport>
-      <ScrollArea.Scrollbar />
-    </ScrollArea.Root>
+    <VStack h="100%" w="100%" align="start" gap="24px">
+      <Flex w="100%" justify="space-between" align="center">
+        <Text>{frozenItems.length} objects found</Text>
+      </Flex>
+      <ScrollArea.Root variant="always">
+        <ScrollArea.Viewport>
+          <ScrollArea.Content paddingEnd="3" textStyle="sm">
+            <VStack gap="16px" align="start" w="100%">
+              <For each={frozenItems}>
+                {(item) => (
+                  <VStack
+                    align="start"
+                    p="8px"
+                    borderRadius="4px"
+                    w="100%"
+                    border="1px solid"
+                    borderColor="gray.200"
+                    key={item.objectID}
+                    gap="8px"
+                  >
+                    <Text>{item.name}</Text>
+                    <Text>Price: {item.price}</Text>
+                    <Text color="gray.500">{item.categories.join(", ")}</Text>
+                  </VStack>
+                )}
+              </For>
+            </VStack>
+          </ScrollArea.Content>
+        </ScrollArea.Viewport>
+        <ScrollArea.Scrollbar />
+      </ScrollArea.Root>
+    </VStack>
   );
 };
